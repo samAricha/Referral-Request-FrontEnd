02f16cb88b4c702e735d00a29d54c8491b61e32d
samAricha <samaricha001@gmail.com>
2023-09-15 13:12:32 +0300

    updating footer to include USAID Health-IT

--- system/resources/views/includes/footer.blade.php
+++ system/resources/views/includes/footer.blade.php
@@ -1,5 +1,5 @@
 <footer id="footer" class="footer">
     <div class="copyright">
-        <h2 style="font-size: 90% !important;" class="card-title">&copy; Copyright <strong><span><a href="https://github.com/TUM-HIS">USAID HEALTH-IT TUM CPU and TUM-HIS CLUB</a></span></strong>. All Rights Reserved</h2>
+        <h2 style="font-size: 90% !important;" class="card-title">&copy; Copyright <strong><span><a href="https://github.com/TUM-HIS">USAID HEALTH-IT TUM CPU</a></span></strong>. All Rights Reserved</h2>
     </div>
 </footer><!-- End Footer -->
